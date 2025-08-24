f1b39bff7fa7eeb3222337f160752bced5dd1598
qMaxXen <x.nesatr@gmail.com>
2025-08-24 20:56:13 +0200

    v2.1.6

--- NBTrackr-Notif.py
+++ NBTrackr-Notif.py
@@ -15,7 +15,7 @@ from colorama import Fore, Style
 
 # Program Version
 
-APP_VERSION = "v2.1.5"
+APP_VERSION = "v2.1.6"
 
 def get_latest_github_release_version():
     url = "https://api.github.com/repos/qMaxXen/NBTrackr/releases/latest"
