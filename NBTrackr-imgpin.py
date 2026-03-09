04e7156fd43a9a605d6b6ff1918cf1123660e6b1
qMaxXen <x.nesatr@gmail.com>
2026-03-09 20:14:28 +0100

    Bump version

--- NBTrackr-imgpin.py
+++ NBTrackr-imgpin.py
@@ -16,7 +16,7 @@ import tarfile
 import sys
 
 # Program Version
-APP_VERSION = "v2.3.1"
+APP_VERSION = "v2.4.0"
 
 CONFIG_DIR = os.path.expanduser("~/.config/NBTrackr")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "settings.json")
